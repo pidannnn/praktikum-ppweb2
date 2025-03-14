ec5fcc5084e7526091b9b7d6f6a8ad2bee79669e
Fi'adn Fawzul Adzhim <fiadn.fawzul.adzhim@gmail.com>
2025-03-14 10:35:02 +0700

    Membuat buku tamu sederhana

---
+++ praktikum_03/kunjungan.php
@@ -0,0 +1,70 @@
+<?php
+require_once("bukuTamu.php");
+session_start();
+
+if(!isset($_SESSION['bukutamu'])){
+    $_SESSION['bukutamu'] = [];
+}
+
+if(isset($_POST['submit'])){
+    // Membuat objek dari class Buku Tamu
+    $bukutamu = new BukuTamu();
+
+    // Mengisi properti timestamp dengan fungsi date
+    // Y-m-d H:i:s artinya 2025-03-13 10:00:00
+    date_default_timezone_set('Asia/Jakarta');
+    $bukutamu->timestamp = date('Y-m-d H:i:s');
+
+    $bukutamu->fullname = $_POST['fullname'];
+    $bukutamu->email = $_POST['email'];
+    $bukutamu->message = $_POST['message'];
+
+    array_push($_SESSION['bukutamu'], $bukutamu);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container my-5">
+        <h2 class="mb-4">Daftar Kunjungan</h2>
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>Timestamp</th>
+                    <th>Full Name</th>
+                    <th>Email</th>
+                    <th>Message</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php foreach($_SESSION["bukutamu"] as $entry): ?>
+                    <tr>
+                        <td>
+                            <?php echo htmlspecialchars($entry->timestamp); ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($entry->fullname); ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($entry->email); ?>
+                        </td>
+                        <td>
+                            <?php echo htmlspecialchars($entry->message); ?>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </tbody>
+        </table>
+        <a href="index.php">&lt;&lt;&lt; Kembali ke halaman utama</a>
+    </div>
+</body>
+</html>
